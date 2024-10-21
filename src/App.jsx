87152fa3f6e6d8b1eb30dae85a8e5e7e79f81a86
Abdelrahman Qamhia <aaq225@lehigh.edu>
2024-10-20 21:26:57 -0400

    UI Desing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-const App = () => {
-  return (
-    <div className=''>App</div>
-  )
-}
+import List from "./components/list/List.jsx";
+import Chat from "./components/chat/Chat.jsx";
+import Detail from "./components/detail/Detail.jsx";
+import Login from "./components/login/Login.jsx";
+import { useState } from "react";
+import Notification from "./components/notification/Notification.jsx";
 
-export default App
+const App = () => {
+  const user = true;
+
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification/>
+    </div>
+  );
+};
+
+export default App;
